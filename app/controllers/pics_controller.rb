0bc535134b645228d4cb96a9e68d4e2a119cc5c4
macunaima <gcgamer46@gmail.com>
2020-10-29 11:59:27 -0300

    actions to create new pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,4 +3,25 @@ class PicsController < ApplicationController
   def index
     
   end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+
+    if @pic.save
+      redirect_to @pic, notice: "A new photo was added!"
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+    def pic_params
+      params.require(:pic).permit(:title, :description)
+    end
 end
