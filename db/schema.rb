c68cb0fb01fc28638d8272c93e2d495dd3386810
macunaima <gcgamer46@gmail.com>
2020-11-03 11:10:30 -0300

    Added association between Pics and User models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_132007) do
+ActiveRecord::Schema.define(version: 2020_11_03_132650) do
 
   create_table "pics", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
