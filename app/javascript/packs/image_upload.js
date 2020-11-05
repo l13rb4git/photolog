d722ad3bfbac38097ddd7e477f57146383013993
macunaima <gcgamer46@gmail.com>
2020-11-05 14:05:00 -0300

    Added a styled button to upload and a preview image area

---
+++ app/javascript/packs/image_upload.js
@@ -0,0 +1,29 @@
+$(function () {
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#imageResult')
+                    .attr('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $('#upload').on('change', function () {
+        var input = document.getElementById( 'upload' );
+        readURL(input);
+    });
+});
+
+window.onload = function () {
+  var input = document.getElementById( 'upload' );
+  var infoArea = document.getElementById( 'upload-label' );
+
+  input.addEventListener( 'change', showFileName );
+  function showFileName( event ) {
+    var input = event.srcElement;
+    var fileName = input.files[0].name;
+    infoArea.textContent = 'File name: ' + fileName;
+  }
+};
