b224c0ef100d3254d35bafb85d0b0914b6062df4
macunaima <gcgamer46@gmail.com>
2020-11-05 15:43:16 -0300

    showing the current image attached on edit page

--- app/javascript/packs/image_upload.js
+++ app/javascript/packs/image_upload.js
@@ -1,19 +1,42 @@
-$(function () {
-    function readURL(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
+function showCurrentImage () {
+  var current_image = document.getElementById( 'current-image' );
 
-            reader.onload = function (e) {
-                $('#imageResult')
-                    .attr('src', e.target.result);
-            };
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
+  if (current_image != null) {
+    var current_image_src = current_image.getAttribute('src');
+
+    $('#imageResult')
+        .attr('src', current_image_src);
+  }
+};
+
+$(function () {
+  showCurrentImage();
+
+  document.addEventListener("turbolinks:load", showCurrentImage)
+
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('#imageResult')
+                  .attr('src', e.target.result);
+          };
+          reader.readAsDataURL(input.files[0]);
+      }
+  }
+
+  function showPreviewImage () {
     $('#upload').on('change', function () {
         var input = document.getElementById( 'upload' );
         readURL(input);
     });
+  }
+
+  showPreviewImage();
+
+  document.addEventListener("turbolinks:load", showPreviewImage)
+
 });
 
 window.onload = function () {
@@ -26,4 +49,6 @@ window.onload = function () {
     var fileName = input.files[0].name;
     infoArea.textContent = 'File name: ' + fileName;
   }
+
 };
+
